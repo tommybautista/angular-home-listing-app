8d48bafbb5cf4c904051274b1b104e405f6c6388
Tommy Bautista <tommybautista320@gmail.com>
2023-11-07 17:09:28 -0500

    completed lesson 10

--- home-listings/src/app/app.component.ts
+++ home-listings/src/app/app.component.ts
@@ -1,24 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     template: `
-    <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
-    <router-outlet></router-outlet>
-  `,
+`,
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, HomeComponent]
+    imports: [
+      CommonModule, 
+      RouterOutlet, 
+      HomeComponent, 
+      RouterModule
+    ]
 })
 export class AppComponent {
   title = 'home-listings';
