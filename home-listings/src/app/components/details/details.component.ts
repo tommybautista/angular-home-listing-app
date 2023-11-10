78088d6488ba018c865f919d37ebb7b722b87a1a
Tommy Bautista <tommybautista320@gmail.com>
2023-11-09 21:48:25 -0500

    lesson 11

--- home-listings/src/app/components/details/details.component.ts
+++ home-listings/src/app/components/details/details.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
+  template: `<p>details works! {{ housingLocationId }}</p>`,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+      this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
